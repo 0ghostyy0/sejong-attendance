c1668a70538ea7ca54336fd71bf15fbd005c4a45
Jinhyung Lee <luciancah@gmail.com>
2022-10-10 00:59:50 +0900

    fix: Bug on Weeknumbercounter when month > 9

--- SejongAttendanceV1/utils/weekNumberCounter.js
+++ SejongAttendanceV1/utils/weekNumberCounter.js
@@ -1,9 +1,10 @@
 const getCurrentDate = () => {
   let time = new Date();
   let month, date, hour, min;
+
   time.getMonth() + 1 < 10
     ? (month = '0' + String(time.getMonth() + 1))
-    : String(time.getMonth() + 1);
+    : (month = String(time.getMonth() + 1));
   time.getDate() < 10
     ? (date = '0' + String(time.getDate()))
     : (date = String(time.getDate()));
@@ -19,6 +20,7 @@ const getCurrentDate = () => {
 
 const weekNumberCounter = () => {
   let currentDate = getCurrentDate();
+  console.log(currentDate);
   let weekNumber,
     i = 1;
   let weeks = [
